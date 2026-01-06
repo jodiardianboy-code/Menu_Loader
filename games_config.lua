00c66f05ab38fe5d570c5abfae1ade0cb55d8b12
jodiardianboy-code <jodiardianboy@gmail.com>
2026-01-06 23:40:31 +0700

    Add files via upload

---
+++ games_config.lua
@@ -0,0 +1,25 @@
+{
+    GAMES = {
+        {
+            name = "🎃 Grim Soul: Dark Fantasy",
+            link = "https://pastebin.com/raw/42Z1fhCb",
+            version = "7.9.0"
+        },
+        {
+            name = "🧟 Last Day on Earth: Survival",
+            link = "https://pastebin.com/raw/REppJphf",
+            version = "1.43.0"
+        },
+        {
+            name = "🚇 Metro 2050 Survival [SOON]",
+            link = "",
+            version = "0.0.365"
+        }
+    },
+    
+    DEVELOPER = {
+        name = "@DraboyGaming",
+        youtube = "@DraboyGaming",
+        Telegram = "@DraboyGaming"
+    }
+}
---
+++ user_config.lua
@@ -0,0 +1,11 @@
+{
+    PASSWORDS = {
+        -- PERMANENT PASSWORDS
+        "DRABOY",
+        "VIP2024",
+        
+        -- TRIAL PASSWORDS
+        {"TRIAL", "2026-01-04"},
+        {"NEWUSER", "2024-12-25"}
+    }
+}
