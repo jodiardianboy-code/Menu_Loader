3c4d9353c1fb77a5c9aa800ad8b5c55b1e7030d6
jodiardianboy-code <jodiardianboy@gmail.com>
2026-01-07 00:12:01 +0700

    Change password entries to uppercase
    Updated password entries to use uppercase letters.

--- user_config.lua
+++ user_config.lua
@@ -1,9 +1,10 @@
 return {
     PASSWORDS = {
-        "joday",
+        "JODAY",
         "VIP202418272",
 
-        {"banten", "2026-01-31"},
+        {"BANTEN", "2026-01-31"},
         {"NEWUSER252736", "2024-12-25"}
     }
 }
+
