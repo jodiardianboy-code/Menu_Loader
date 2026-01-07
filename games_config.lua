c3fdf86405815fc2e6a4c9ec978a5fc1f3866694
jodiardianboy-code <jodiardianboy@gmail.com>
2026-01-07 08:54:14 +0700

    Refactor games_config.lua to return a table
    Updated the structure of games_config.lua to return a table instead of using a Lua table literal.

--- games_config.lua
+++ games_config.lua
@@ -1,4 +1,4 @@
-{
+return {
     GAMES = {
         {
             name = "Grim Soul",
@@ -10,17 +10,5 @@
             version = "1.43.0",
             link = "https://pastebin.com/raw/REppJphf"
         }
-    },
-
-    NEWS = [[
-🔥 DRABOY LOADER NEWS 🔥
-
-• Welcome to Cloud Loader v1  
-• New scripts coming soon  
-• Grim Soul updated  
-• Metro 2050 in development  
-
-Follow @DraboyGaming
-]]
+    }
 }
-
