655d231dc5b45883572dbf5f1431fc6e848c69b6
jodiardianboy-code <jodiardianboy@gmail.com>
2026-01-07 08:53:41 +0700

    Add news configuration for Draboy Cloud Loader

---
+++ news_config.lua
@@ -0,0 +1,13 @@
+return {
+    TITLE = "🔥 DRABOY CLOUD NEWS 🔥",
+    MESSAGE = [[
+Welcome to Draboy Cloud Loader
+
+• Grim Soul updated
+• LDOE mod coming
+• Metro 2050 in development
+
+Follow @DraboyGaming
+    ]],
+    LAST_UPDATE = "2026-01-07"
+}
