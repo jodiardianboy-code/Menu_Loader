ec19c3ac9bdf2b5d65bedda8abee70eac298b2b5
jodiardianboy-code <jodiardianboy@gmail.com>
2026-01-18 17:16:19 +0700

    Add files via upload

---
+++ news_configv2.lua
@@ -0,0 +1,36 @@
+
+
+
+{
+    TITLE = "🔥 DRABOY NEWS 🔥",
+
+    PAGES = {
+        {
+            title = "Update Januari 2026",
+            content = [[
+• New cloud loader
+• Login system improved
+• Faster refresh server
+• UI cleaner
+]]
+        },
+
+        {
+            title = "Fix & Improvement",
+            content = [[
+• Fixed script ended
+• Better exit confirm
+• News modular system
+]]
+        },
+
+        {
+            title = "Coming Soon",
+            content = [[
+• Game info page
+• Mod list preview
+• Account binding
+]]
+        }
+    }
+}
